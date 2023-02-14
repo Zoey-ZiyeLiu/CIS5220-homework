22fdc8fc560e243cdb87a509fc43dddef306a0c6
Zoey-ZiyeLiu <liuziye@seas.upenn.edu>
2023-02-14 16:01:45 -0500

    strange

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -15,7 +15,7 @@ class CustomLRScheduler(_LRScheduler):
         last_epoch=-1,
         initial_lr=0.01,
         eta_min=0,
-        T_max=1.5,
+        T_max=2,
     ):
         """
         Create a new scheduler.
