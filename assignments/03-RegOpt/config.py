2cdc2f01762ae7ce2cdffd35807662a261a3cc16
Zoey-ZiyeLiu <liuziye@seas.upenn.edu>
2023-02-14 15:00:20 -0500

    first_try

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,19 +8,25 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 6
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "initial_lr": initial_learning_rate,
+        "eta_min": 0,
+        "T_max": 2,
+        "last_epoch": -1,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,22 @@
 from typing import List
-
+import torch
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Create a nre Scheduler.
+
+    """
+
+    def __init__(
+        self,
+        optimizer,
+        last_epoch=-1,
+        initial_lr=0.01,
+        eta_min=0,
+        T_max=2,
+    ):
         """
         Create a new scheduler.
 
@@ -13,12 +25,42 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+
+        # print("HERERE")
+        self.optimizer = optimizer
+        self.lr = initial_lr
+        self.last_epoch = last_epoch
+        # print(self.last_epoch,"NERE")
+        self.eta_min = eta_min
+        self.T_max = T_max
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        Get New learing rate.
+
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # print(self.base_lrs,"base_lrs")
+        # print("here")
+        pi = torch.acos(torch.zeros(1)) * 2
+        # print(type(pi))
+        # self.lr=initial_lr
+        # print(self.lr)
+        if self.last_epoch == -1:
+            lr = self.lr
+            # self.last_epoch+=1
+            # print(self.lr,"if")
+        else:
+            # print(self.last_epoch,"epoch")
+            lr = self.eta_min + 0.5 * abs(self.lr - self.eta_min) * (
+                1 + torch.cos(pi * self.last_epoch / self.T_max)
+            )
+            # print(lr,"else")
+            # self.last_epoch+=1
+        # return [i for i in self.lr]
+        return lr
